9536934e30f91e9dcb8c5bc876a09263de760613
wvizmanos <75106803+wvizmanos@users.noreply.github.com>
2026-02-06 10:25:56 +0800

    Set trust proxy for Express app

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+app.set('trust proxy', 1);
 const port = process.env.PORT || 3001;
 
 app.get("/", (req, res) => res.type('html').send(html));
